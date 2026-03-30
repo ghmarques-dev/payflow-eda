7e29134a941a9d4b6ca302f695a53d6cb1c96eb8
Guilherme Marques <guilhermehenrique12308@gmail.com>
2026-03-30 20:03:37 -0300

    feat(sales-service): update SaleCheckoutRequestedEventPayload to use SaleCheckoutItem type for better structure

--- packages/contracts/src/sales/sale-checkout-requested.ts
+++ packages/contracts/src/sales/sale-checkout-requested.ts
@@ -1,7 +1,14 @@
 export interface SaleCheckoutRequestedEventPayload {
   sale_id: string;
-  items: Array<Record<string, number>>;
+  items: Array<SaleCheckoutItem>;
   occurred_at: Date;
+}
+
+export type SaleCheckoutItem = {
+  sale_item_id: string;
+  product_id: string;
+  quantity: number;
+  unit_price_in_cents: number;
 }
 
 export const SALE_CHECKOUT_REQUESTED_EVENT_TYPE = 'sale.checkout_requested';
